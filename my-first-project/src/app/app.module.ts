dc246ff79102bf157d2f67c05104a7f6fa10c9a4
Manish Singh <manish@brillmark.com>
2023-11-01 10:46:19 +0530

    new

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CountryListComponent
   ],
   imports: [
     BrowserModule,
